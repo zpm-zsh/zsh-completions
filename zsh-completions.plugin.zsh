1a80209935d1fd314bc452e12082964a211fbdf5
Grigorii Horos <horosgrisa@gmail.com>
2023-11-26 18:30:48 +0200

    Update completions

--- zsh-completions.plugin.zsh
+++ zsh-completions.plugin.zsh
@@ -1,1 +1,10 @@
-fpath+="${0:A:h}/src"
+#!/usr/bin/env zsh
+
+# Standarized $0 handling, following:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
